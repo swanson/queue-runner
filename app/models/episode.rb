7bfcb2a7914a8c9507e903106bd153807fa666df
Matt Swanson <mdswanson@sep.com>
2012-10-07 00:06:53 -0400

    s1e1 -> s01e01

--- app/models/episode.rb
+++ app/models/episode.rb
@@ -2,6 +2,7 @@ class Episode < ActiveRecord::Base
   belongs_to :show
 
   def to_s
-    "#{show.name} S#{season_number}E#{episode_number}, \"#{name}\" (Aired: #{air_date})"
+    se = "S#{season_number.to_s.rjust(2, '0')}E#{episode_number.to_s.rjust(2, '0')}"
+    "#{show.name} #{se}, \"#{name}\" (Aired: #{air_date})"
   end
 end
