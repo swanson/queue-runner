8ce7375400fbf3d8bf40e28dbb863768c7175cba
Matt Swanson <mdswanson@sep.com>
2012-11-19 20:31:36 -0500

    collapse episodes in show tab, making the page kinda usable

--- app/assets/javascripts/show.js
+++ app/assets/javascripts/show.js
@@ -1,5 +1,17 @@
 $(document).ready(function() {
   $("#new-show form").submit(function(e) {
     $("#new-show").spin({top: "180px"});
-  })
+  });
+
+  $(".show-title").click(function(e) {
+    var icon = $(this).find("i");
+
+    if (icon.hasClass("icon-chevron-right")) {
+      icon.removeClass("icon-chevron-right")
+          .addClass("icon-chevron-down");
+    } else {
+      icon.removeClass("icon-chevron-down")
+          .addClass("icon-chevron-right");
+    }
+  });
 });
