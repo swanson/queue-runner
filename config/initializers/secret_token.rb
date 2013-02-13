92e8957bb1af87122145868580a941115c7434e3
Matt Swanson <mdswanson@sep.com>
2013-02-12 22:21:47 -0500

    move secret to ENV_VAR, closes #9

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,10 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-TvQueue::Application.config.secret_token = 'a1d011cc6a9689425b4be2f4772ea6c231db3f3a7732478643a777fa4d6ec0a0dddd5c04cf0e2eda0a0bc369a7cf2fcd6e2cc9567546674c4f804362c762f25d'
+
+if Rails.env.production? && ENV['SECRET_TOKEN'].blank?
+  raise 'SECRET_TOKEN environment variable must be set!'
+end
+
+TvQueue::Application.config.secret_token =
+  ENV['SECRET_TOKEN'] || 'a1d011cc6a9689425b4be2f4772ea6c231db3f3a7732478643a777fa4d6ec0a0dddd5c04cf0e2eda0a0bc369a7cf2fcd6e2cc9567546674c4f804362c762f25d'
